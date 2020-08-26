2b957914740b8dc28a92286ccc3c009fd52c0aaf
Tyler Wolf <twolf19@huskers.unl.edu>
2020-08-26 02:39:58 +0000

    Initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Tyler Wolf
+ *         twolf19@huskers.unl.edu
+ * Date: 8/25/20
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tyler Wolf!\n");
+  printf("Electrical engineering\n");
+
+  return 0;
+}
